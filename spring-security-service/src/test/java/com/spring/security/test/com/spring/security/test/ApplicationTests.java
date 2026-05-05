47eb0a018abac977a8814a3d288992f28ea53ad7
sumankumarcs92 <109429136+sumankumarcs92@users.noreply.github.com>
2026-05-06 02:10:19 +0530

    spring-security-service

---
+++ spring-security-service/src/test/java/com/spring/security/test/com/spring/security/test/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.security.test.com.spring.security.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
