470dc62c427029b87673e44fc3d1c8ab1c2a0dd0
sumankumarcs92 <109429136+sumankumarcs92@users.noreply.github.com>
2026-07-12 20:22:49 +0530

    Rate limiting code.

--- redis-caching/src/main/java/com/example/controller/EmployeeController.java
+++ redis-caching/src/main/java/com/example/controller/EmployeeController.java
@@ -3,10 +3,16 @@ package com.example.controller;
 import com.example.dto.ResponseVO;
 import com.example.model.Employee;
 import com.example.service.EmployeeService;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
 import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Duration;
 
 @RestController
 @RequestMapping("/emp")
@@ -16,13 +22,19 @@ public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @Autowired
+    private Bucket bucket;
+
+
     @GetMapping("/{id}")
-    public ResponseVO getEmployeeById(@PathVariable("id")Long id ){
+    public Object getEmployeeById(@PathVariable("id")Long id ){
         log.info("EmployeeController:: getEmployeeById:  {}",id);
-       Employee emp = employeeService.getEmployee(id);
-        log.info(" {} ",emp);
-        ResponseVO response = new ResponseVO(emp);
-       return response;
+        if(bucket.tryConsume(1)){
+            ResponseVO response = new ResponseVO(employeeService.getEmployee(id));
+            return response;
+        }
+
+       return ResponseEntity.status(429).body("Too Many Requests");
     }
     @PostMapping("/update")
     public ResponseVO saveOrUpdate(@Valid @RequestBody Employee employee){
@@ -37,5 +49,18 @@ public class EmployeeController {
     public void  deleteEmployeeById(@PathVariable("id")Long id ){
         log.info("EmployeeController:: deleteEmployeeById:  {}",id);
         employeeService.delete(id);
+    }
+
+    @Bean
+    public static Bucket bucket() {
+
+        Bandwidth limit = Bandwidth.builder()
+                .capacity(5)
+                .refillGreedy(5, Duration.ofMinutes(1))
+                .build();
+
+        return Bucket.builder()
+                .addLimit(limit)
+                .build();
     }
 }
